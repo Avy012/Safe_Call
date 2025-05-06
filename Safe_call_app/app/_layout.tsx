bc9c0965e1294ad414915d53294d2c26461b6325
Avy012 <kist0907@gmail.com>
2025-05-07 02:05:22 +0900

    라이브킷 시도중

--- Safe_call_app/app/_layout.tsx
+++ Safe_call_app/app/_layout.tsx
@@ -1,5 +1,8 @@
 import { Stack } from "expo-router";
+import { registerGlobals } from '@livekit/react-native';
 import './globals.css';
+
+registerGlobals();
 
 export default function RootLayout() {
   return <Stack>
