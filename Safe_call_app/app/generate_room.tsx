3e15b0a9bc6cf40cbc1f94e0ad28ce218b44f427
Avy012 <kist0907@gmail.com>
2025-05-11 01:19:24 +0900

    룸 생성 코드 추가

---
+++ Safe_call_app/app/generate_room.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect } from 'react';
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  ListRenderItem,
+} from 'react-native';
+import {
+  AudioSession,
+  LiveKitRoom,
+  useTracks,
+  TrackReferenceOrPlaceholder,
+  VideoTrack,
+  isTrackReference,
+  registerGlobals,
+} from '@livekit/react-native';
+import { Track } from 'livekit-client';
+
+// Setup LiveKit WebRTC support
+registerGlobals();
+
+// Replace with your actual values   생성된 토큰 여기에 넣는걸로 하면 될 듯 
+const wsURL = "wss://safecall-ozn2xsg6.livekit.cloud";
+const token = "your_jwt_token_here";
+
+const LiveKitRoomScreen: React.FC = () => {
+  useEffect(() => {
+    const start = async () => {
+      await AudioSession.startAudioSession();
+    };
+
+    start();
+    return () => {
+      AudioSession.stopAudioSession();
+    };
+  }, []);
+
+  return (
+    <LiveKitRoom
+      serverUrl={wsURL}
+      token={token}
+      connect={true}
+      options={{
+        adaptiveStream: { pixelDensity: 'screen' },
+      }}
+      audio={true}
+      video={true}
+    >
+      <RoomView />
+    </LiveKitRoom>
+  );
+};
+
+const RoomView: React.FC = () => {
+  const tracks = useTracks([Track.Source.Camera]);
+
+  const renderTrack: ListRenderItem<TrackReferenceOrPlaceholder> = ({ item }) => {
+    if (isTrackReference(item)) {
+      return <VideoTrack trackRef={item} style={styles.participantView} />;
+    } else {
+      return <View style={styles.participantView} />;
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList<TrackReferenceOrPlaceholder>
+        data={tracks}
+        renderItem={renderTrack}
+        keyExtractor={(_, index) => index.toString()}
+      />
+    </View>
+  );
+};
+
+export default LiveKitRoomScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  participantView: {
+    height: 300,
+  },
+});
