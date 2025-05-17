9b345b6570932fbf78e7b4ca49bfd294ce04ce48
Avy012 <kist0907@gmail.com>
2025-05-17 22:30:09 +0900

    전화화면 수정 > 음성통화로

--- Safe_call_app/app/generate_room.tsx
+++ Safe_call_app/app/generate_room.tsx
@@ -1,7 +1,7 @@
 // 생성된 룸 입장하는 코드 
 // 토큰 생성한거 가져오는 코드 추가해야 함 
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import {
   StyleSheet,
   View,
@@ -20,7 +20,7 @@ import {
   registerGlobals,
 } from '@livekit/react-native';
 import { Track } from 'livekit-client';
-import { useRouter } from 'expo-router';
+import { useRouter,} from 'expo-router';
 import { icons } from '@/constants/icons';
 
 // Setup LiveKit WebRTC support
@@ -28,9 +28,10 @@ registerGlobals();
 
 // Replace with your actual values   생성된 토큰 여기에 넣는걸로 하면 될 듯 
 const wsURL = "wss://safecall-ozn2xsg6.livekit.cloud";
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoibXkgbmFtZSIsInZpZGVvIjp7InJvb21Kb2luIjp0cnVlLCJyb29tIjoibXktcm9vbSIsImNhblB1Ymxpc2giOnRydWUsImNhblN1YnNjcmliZSI6dHJ1ZSwiY2FuUHVibGlzaERhdGEiOnRydWV9LCJzdWIiOiJpZGVudGl0eSIsImlzcyI6IkFQSXE2Q1Y1ZTM3N2hteiIsIm5iZiI6MTc0NzM4NzQ0NywiZXhwIjoxNzQ3NDA5MDQ3fQ.94SzrSym85OhqwVBSpOAwLswR2KGimjDgs4nMk0-O4g";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZGtpbSIsInZpZGVvIjp7InJvb21Kb2luIjp0cnVlLCJyb29tIjoibXktcm9vbSIsImNhblB1Ymxpc2giOnRydWUsImNhblN1YnNjcmliZSI6dHJ1ZSwiY2FuUHVibGlzaERhdGEiOnRydWV9LCJzdWIiOiJpZGVudGl0eSIsImlzcyI6IkFQSXE2Q1Y1ZTM3N2hteiIsIm5iZiI6MTc0NzQ4NzE4NiwiZXhwIjoxNzQ3NTA4Nzg2fQ.6Bo0WiIbyehdGrDrLGrWaNO_WjxIY00-iRh1WwfT6Ms";
 
 const LiveKitRoomScreen: React.FC = () => {
+  // const roomRef = useRef<typeof LiveKitRoom | null>(null);  // Use ref to store room instance
   useEffect(() => {
     const start = async () => {
       await AudioSession.startAudioSession();
@@ -39,7 +40,10 @@ const LiveKitRoomScreen: React.FC = () => {
     start();
     return () => {
       AudioSession.stopAudioSession();
+      console.log(token)
     };
+
+    
   }, []);
 
   return (
@@ -52,11 +56,30 @@ const LiveKitRoomScreen: React.FC = () => {
       }}
       audio={true}
       video={true}
+      // onConnected={() => {
+      //   // Access the room instance after connection
+      //   if (roomRef.current) {
+      //     console.log("Connected to room:", roomRef.current);
+
+      //     // Use track events to manage media
+      //     roomRef.current.prototype.onTrackSubscribed = (track) =>  {
+      //       console.log('Track Subscribed:', track);
+      //     };
+
+      //     roomRef.current.prototype.onTrackSubscribed = (track) => {
+      //       console.log('Track Unsubscribed:', track);
+      //     };
+      //   }
+      // }}
+      // onError={(error) => {
+      //   console.log('Error in connecting to the room:', error);
+      // }}
     >
       <RoomView />
     </LiveKitRoom>
   );
 };
+
 
 const RoomView: React.FC = () => {
   const tracks = useTracks([Track.Source.Camera]);
@@ -64,7 +87,7 @@ const RoomView: React.FC = () => {
 
   const renderTrack: ListRenderItem<TrackReferenceOrPlaceholder> = ({ item }) => {
     if (isTrackReference(item)) {
-      return <VideoTrack trackRef={item} style={styles.participantView} />;
+      return <View style={styles.participantView} />; //여기서 영상 띄우면 영상으로 나가고 View로 띄우면 영상화면안나감 )
     } else {
       return <View style={styles.participantView} />;
     }
@@ -78,7 +101,7 @@ const RoomView: React.FC = () => {
         keyExtractor={(_, index) => index.toString()}
       />
       {/* End Call Button */}
-      <ImageBackground source={icons.hangup} className="w-[70px] h-[70px] mx-2 rounded-xl overflow-hidden absolute left-1/2 -translate-x-1/2">
+      <ImageBackground source={icons.hangup} className="w-[70px] h-[70px] mx-2 rounded-xl overflow-hidden absolute left-1/2 -translate-x-1/2 bottom-40">
         <TouchableOpacity onPress={() => router.back()} className="w-full h-full"/>
       </ImageBackground>
     </View>
@@ -94,8 +117,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'stretch',
     justifyContent: 'center',
+    backgroundColor : '#000000'
   },
   participantView: {
     height: 300,
+    
   },
 });
