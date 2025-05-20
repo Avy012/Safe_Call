5a41f69cf0c5d46ebcd90fa5bd91c3a7b9943c34
Avy012 <kist0907@gmail.com>
2025-05-20 20:55:14 +0900

    text 오류 수정

--- Safe_call_app/app/_layout.tsx
+++ Safe_call_app/app/_layout.tsx
@@ -1,30 +1,18 @@
 // app/_layout.tsx
 
 import React from 'react';
-import { Stack } from 'expo-router';
+import { Slot } from 'expo-router';
 import { registerGlobals } from '@livekit/react-native';
-import { UserProvider } from '@/context/UserContext'; // 이 줄 추가
+import { UserProvider } from '@/context/UserContext';
 import './globals.css';
-
 
 registerGlobals();
 
-
 export default function RootLayout() {
   return (
-    <UserProvider> {/*  전체 앱을 Context로 감싸*/}
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="keypad" options={{ headerShown: false }} />
-        <Stack.Screen name="callScreen" options={{ headerShown: false }} /> 
-        <Stack.Screen name="settings" options={{ headerShown: false }} /> 
-        <Stack.Screen name="account" options={{ headerShown: false }} />
-        <Stack.Screen name="privacy" options={{ headerShown: false }} />
-        <Stack.Screen name="notification" options={{ headerShown: false }} />
-        <Stack.Screen name="IncomingCallScreen" options={{ headerShown: false }} />
-        <Stack.Screen name="calls/[callId]" options={{ headerShown: false }} />
-        <Stack.Screen name="calls/CallSummary" options={{ headerShown: false }} />
-      </Stack>
+    <UserProvider>
+      <Slot />
     </UserProvider>
   );
 }
+
