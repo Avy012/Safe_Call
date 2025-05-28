82b04d1575a2dc6e02a8c096b7935d8e891b3c3e
Avy012 <kist0907@gmail.com>
2025-05-28 10:57:15 +0900

    notification file to notificationPY

--- Safe_call_app/app/server.py
+++ Safe_call_app/app/server.py
@@ -4,7 +4,7 @@ import time
 import jwt
 from fastapi import FastAPI, Request
 from dotenv import load_dotenv
-from notification import send_push_notification  # ✅ import this
+from notificationPY import send_push_notification  # ✅ import this
 from firebase_admin import firestore, credentials, initialize_app
 import firebase_admin
 
